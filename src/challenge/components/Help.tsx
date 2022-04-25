8db63eea1104a30e6e747f03e2e91d8588f11133
Gyuri <gdenes355@gmail.com>
2022-04-25 19:18:28 +0100

    reformat code

--- src/challenge/components/Help.tsx
+++ src/challenge/components/Help.tsx
@@ -102,21 +102,19 @@ const Help = (props: HelpProps) => {
               <code>F5</code>
             </td>
             <td>Stop debugging</td>
-          </tr>          
+          </tr>
         </table>
         <h4>Notes</h4>
         <table>
           <tr>
-            <td>
-              Debugging requires breakpoints to be set.
-            </td>
+            <td>Debugging requires breakpoints to be set.</td>
           </tr>
           <tr>
             <td>
               Run will ignore breakpoints but be quicker in execution time.
-            </td>          
-          </tr>          
-        </table>        
+            </td>
+          </tr>
+        </table>
       </Styled>
     </Fade>
   );
