65efe8dc97d486eb7f77e9109c3ece485ce982fb
gdenes <gdenes355@gmail.com>
2023-11-14 21:35:31 +0000

    support ansi format characters in the console

--- src/components/Console.tsx
+++ src/components/Console.tsx
@@ -4,10 +4,13 @@ import React, {
   KeyboardEvent,
   MouseEvent,
   useContext,
+  useState,
 } from "react";
 import { Box } from "@mui/material";
 import "./Console.css";
 import VsThemeContext from "../themes/VsThemeContext";
+import Anser from "anser";
+import parse from "html-react-parser";
 
 type ConsoleProps = {
   isInputEnabled: boolean;
@@ -19,10 +22,21 @@ type ConsoleProps = {
 // max character count in the console
 const MAX_LENGTH = 1000000;
 
+const transformText = (text: string) => {
+  text = text.replace("\n", "\r\n");
+  text = text.slice(-MAX_LENGTH);
+  if (text.includes("\u001b")) {
+    return parse(Anser.ansiToHtml(text));
+  }
+  return text;
+};
+
 const Console = (props: ConsoleProps) => {
-  const containerEl = useRef<HTMLDivElement>(null);
+  const containerEl = useRef<HTMLDivElement | null>(null);
   const inputFieldEl = useRef<HTMLInputElement>(null);
   const vsThemeContext = useContext(VsThemeContext);
+
+  const [fontSize, setFontSize] = useState<number>(15);
 
   const onKeyPressed = (event: KeyboardEvent) => {
     if (inputFieldEl.current && event.key === "Enter") {
@@ -52,6 +66,17 @@ const Console = (props: ConsoleProps) => {
     }
   }, [props]);
 
+  const onWheel = (event: WheelEvent) => {
+    if (event && event.ctrlKey) {
+      if (event.deltaY > 0) {
+        setFontSize((prev) => Math.max(2, prev - 1));
+      } else {
+        setFontSize((prev) => Math.min(72, prev + 1));
+      }
+      event.preventDefault();
+    }
+  };
+
   useEffect(() => {
     if (props.isInputEnabled && inputFieldEl.current) {
       inputFieldEl.current.textContent = "";
@@ -59,18 +84,23 @@ const Console = (props: ConsoleProps) => {
     }
   }, [props.isInputEnabled]);
 
+  const setRef = (el: HTMLDivElement) => {
+    if (containerEl.current) {
+      containerEl.current.removeEventListener("wheel", onWheel);
+    }
+    if (el) el.addEventListener("wheel", onWheel, { passive: false });
+    containerEl.current = el;
+  };
+
   return (
     <Box sx={{ width: "100%", height: "100%", bgcolor: "background.default" }}>
       <div
         className={"console theme-" + vsThemeContext.theme}
-        ref={containerEl}
+        ref={setRef}
         onClick={onClick}
+        style={{ fontSize: fontSize + "px" }}
       >
-        <span className="printed-span">
-          {props.content.length > MAX_LENGTH
-            ? props.content.replace("\n", "\r\n").slice(0, MAX_LENGTH)
-            : props.content.replace("\n", "\r\n")}
-        </span>
+        <span className="printed-span">{transformText(props.content)}</span>
         <span
           className={!props.isInputEnabled ? "input-span hidden" : "input-span"}
           ref={inputFieldEl}
